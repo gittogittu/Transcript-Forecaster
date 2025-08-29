99866b1704c24abfaefc2f54a9aadf52565857de
aditya <aditya@oneorigin.us>
2025-08-29 18:39:40 +0530

    bug fi

---
+++ src/lib/database/migrations/003_update_transcripts_schema.sql
@@ -0,0 +1,48 @@
+OTHING;DO Nersion) (vFLICT e')
+ON CON codlicationatch appema to mtable schnscripts  'Update traa',pts_schemscrite_tran('003_updaUES 
+on) VALcriptideson, ns (versi migratioNSERT INTO
+Iordion recratInsert mig- by);
+
+-ted_ripts(creascON trancreated_by ranscripts_idx_t EXISTS  NOTE INDEX IFEATCRpe);
+_typtpts(transcricritransN  Ocripts_typeS idx_transOT EXISTIF NNDEX 
+CREATE I date);id,(client_ptscriON transe _datntliecripts_cransx_tidTS OT EXISE INDEX IF N
+CREATdate);s(pttranscripts_date ON dx_transcriNOT EXISTS i IF  INDEXREATEth;
+Cent_moncripts_cliransS idx_tSTDEX IF EXIOP INmonth;
+DRanscripts_STS idx_trNDEX IF EXIDROP Ite indexes
+ Upda);
+
+--dateclient_id, E(IQU_key UNient_id_dates_cltranscript CONSTRAINT ipts ADD transcrBLEALTER TAey;
+h_kient_id_montts_clranscripS tXISTTRAINT IF Ets DROP CONScripransABLE tER Tstraint
+ALTunique conate t;
+
+-- UpdEXISTS counOLUMN IF  Cpts DROPanscriBLE trTER TA ALh;
+-- montIF EXISTSUMN  DROP COLptsBLE transcriER TA
+-- ALTon)a migrati datverifying after entcommety - un for safented outmns (commcolu- Drop old  >= 0);
+
+-countcript_ns (traCK
+CHEve itit_count_posnscripk_traT checD CONSTRAINripts 
+ADABLE transcALTER Tunt
+anscript_coraint for trck constAdd che
+
+--  NOT NULL;_count SETnscriptR COLUMN tra,
+ALTESET NOT NULLe LUMN dat
+ALTER COtranscripts BLE LTER TAmigration
+Aer data ULL afts NOT Nolumn Make new c
+--IS NULL;
+t_count  transcrip IS NULL ORte da
+WHEREnt = countcript_couansnth,
+  tr date = mo
+ pts 
+SET transcriATE olumns
+UPDs to new cold columnfrom  data pyD;
+
+-- CoUIted_by UISTS creaMN IF NOT EXD COLU(100),
+ADe VARCHARtypranscript_STS tT EXIF NOOLUMN IADD Ct INTEGER,
+ript_counTS transc IF NOT EXISADD COLUMN,
+e DATES datT EXIST NOMN IF COLUts 
+ADDcripBLE trans
+ALTER TAw columns- Add neaces
+
+-erfcript intmatch TypeScolumns to s table ipttranscre n: Updatriptio
+-- DescVersion: 003- de
+-n coioh applicattcto maable schema  ttranscriptspdate igration: U-- M
